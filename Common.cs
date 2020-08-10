6111eadbcf17db49d325f16c321abd1b1395995b
kylegrussell <kyle.graham.russell@gmail.com>
2020-08-10 16:21:26 -0400

    Added Common class to generate blobclient

---
+++ Common.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using Microsoft.Azure.Storage.Auth;
+namespace helloazurestoragedemo
+{
+    public class Common
+    {
+        public static CloudBlobClient CreateBlobClientFromAccountAndKey(string accountName, string keyValue)
+        {
+            CloudStorageAccount storageAccount;
+            CloudBlobClient blobClient;
+
+            try
+            {
+                StorageCredentials credentials = new StorageCredentials(accountName, keyValue);
+                storageAccount = new CloudStorageAccount(credentials, accountName, null, true);
+                blobClient = storageAccount.CreateCloudBlobClient();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
+            return blobClient;
+        }
+    }
+}
