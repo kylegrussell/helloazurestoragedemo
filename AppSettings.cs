115ae60c0dcc1e5cd4c748cbbb1fd8813d39c8f8
kylegrussell <kyle.graham.russell@gmail.com>
2020-08-10 17:04:37 -0400

    Added json config and basic usage

---
+++ AppSettings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+
+namespace helloazurestoragedemo
+{
+    public class AppSetttings
+    {
+        public string SourceSASToken { get; set; }
+        public string SourceAccountName { get; set; }
+        public string SourceContainerName { get; set; }
+        public string DestinationSASToken { get; set; }
+        public string DestinationAccountName { get; set; }
+        public string DestinationContainerName { get; set; }
+        public object Name { get; set; }
+
+        public static AppSetttings LoadAppSettings()
+        {
+            IConfigurationRoot configurationRoot = new ConfigurationBuilder()
+                .AddJsonFile("AppSettings.json", false)
+                .Build();
+
+            AppSetttings appSetttings = configurationRoot.Get<AppSetttings>();
+
+            return appSetttings;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Configuration;
+using Microsoft.Azure.Storage.Blob;
 
 namespace helloazurestoragedemo
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //await GetSystemProperties();
+            await GetName();
+        }
+
+        private static async Task GetName()
+        {
+            AppSetttings appSettings = AppSetttings.LoadAppSettings();
+
+            Console.WriteLine("Name : " + appSettings.Name);
+        }
+
+        private static async Task GetSystemProperties()
+        {
+            AppSetttings appSettings = AppSetttings.LoadAppSettings();
+
+            CloudBlobClient blobClient = Common.CreateBlobClientFromAccountAndKey(appSettings.SourceAccountName, appSettings.SourceSASToken);
+            CloudBlobContainer container = blobClient.GetContainerReference(appSettings.SourceContainerName);
+            await container.CreateIfNotExistsAsync();
+
+            container.FetchAttributes();
+
+            Console.WriteLine($"Properties for container {container.StorageUri.PrimaryUri.ToString()}");
+            Console.WriteLine($"Etag: {container.Properties.ETag}");
+            Console.WriteLine($"Last modified: {container.Properties.LastModified.ToString()}");
+            Console.WriteLine($"Lease Status: {container.Properties.LeaseStatus.ToString()}");
+
+            container.Metadata.Add("department", "Technical");
+            container.Metadata["category"] = "Knowledge Base";
+            container.Metadata.Add("docType", "txtDocuments");
+
+            container.SetMetadata();
+
+            container.FetchAttributes();
+            foreach (var item in container.Metadata)
+            {
+                Console.WriteLine($"Key: {item.Key}");
+                Console.WriteLine($"Value: {item.Value}");
+            }
         }
     }
 }
