6f66078b9d4afb1a4ed4a03cd3d86c5fd8ee504f
kylegrussell <kyle.graham.russell@gmail.com>
2020-08-10 15:46:30 -0400

    Initial Hello World code

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace helloazurestoragedemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
